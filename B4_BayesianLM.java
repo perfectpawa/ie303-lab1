889af1e00ded719be961dc8dae96e705191393e9
perfectpawa <baotlg1005@gmail.com>
2025-03-18 13:27:13 +0700

    B4 - sentence still weird

--- B4_BayesianLM.java
+++ B4_BayesianLM.java
@@ -1,5 +1,6 @@
-import java.util.*;
 import java.io.*;
+import java.text.Normalizer;
+import java.util.*;
 
 public class B4_BayesianLM {
 
@@ -12,8 +13,8 @@ public class B4_BayesianLM {
     public static void readFile() {
         try {
             Vector<String> lines = new Vector<String>();
-            File file = new File("UIT-ViOCD/UIT-ViOCD.txt");
-            Scanner fileScanner = new Scanner(file);
+            File file = new File("UIT-ViOCD.txt");
+            Scanner fileScanner = new Scanner(file, "UTF-8");
 
             while (fileScanner.hasNextLine()) {
                 String line = fileScanner.nextLine();
@@ -21,7 +22,8 @@ public class B4_BayesianLM {
             }
             fileScanner.close();
 
-            for (String line: lines) {
+            for (String line : lines) {
+                line = Normalizer.normalize(line, Normalizer.Form.NFC);
                 // remove line break \n, \r and tab \t
                 line = line.replace("\n", "").replace("\r", "").replace("\t", "");
                 // remove all leading spaces
@@ -34,11 +36,10 @@ public class B4_BayesianLM {
                 // collecting words
                 int wordId = 0;
                 String[] words = line.split("\\s+");
-                for (String word: words) {
+                for (String word : words) {
                     if (corpus.containsKey(word)) {
                         corpus.put(word, corpus.get(word) + 1);
-                    }
-                    else {
+                    } else {
                         vocab.put(word, wordId);
                         corpus.put(word, 1);
                         wordId += 1;
@@ -47,17 +48,22 @@ public class B4_BayesianLM {
 
                 // collecting pairs of words
                 for (int i = 0; i < words.length - 1; i++) {
-                    String words_ij = words[i] + "_" + words[i+1];
+                    String words_ij = words[i] + "_" + words[i + 1];
                     if (pairCorpus.containsKey(words_ij)) {
                         pairCorpus.put(words_ij, pairCorpus.get(words_ij) + 1);
-                    }
-                    else {
+                    } else {
                         pairCorpus.put(words_ij, 1);
                     }
                 }
             }
-        }
-        catch (FileNotFoundException fileNotFoundException) {
+            //check is "hàng" in a vocab
+            // if (vocab.containsKey("hàng")) {
+            //     System.out.println("hàng is in vocab");
+            // } else {
+            //     System.out.println("hàng is not in vocab");
+            // }
+
+        } catch (FileNotFoundException fileNotFoundException) {
             System.out.println("File not found!");
             fileNotFoundException.printStackTrace();
         }
@@ -66,82 +72,88 @@ public class B4_BayesianLM {
     public static void constructSingleProb() {
         // determine the total number of words in the dataset
         int totalWords = 0;
-        for (Map.Entry<String, Integer> item: corpus.entrySet()) {
+        for (Map.Entry<String, Integer> item : corpus.entrySet()) {
             totalWords += item.getValue();
         }
 
         // calculating the probability of each word
         probs = new Double[vocab.size()];
-        for (Map.Entry<String, Integer> item: corpus.entrySet()) {
+        for (Map.Entry<String, Integer> item : corpus.entrySet()) {
             String word = item.getKey();
             Integer wordCount = corpus.get(word);
 
             Integer wordId = vocab.get(word);
-            
+
             // determining the P(w)
+            probs[wordId] = (double) wordCount / totalWords;
         }
     }
 
     public static void constructConditionalProb() {
-        // determine the total number of words in the dataset
         int totalPairsOfWords = 0;
-        for (Map.Entry<String, Integer> item: pairCorpus.entrySet()) {
-            totalPairsOfWords += item.getValue();
+        for (Map.Entry<String, Integer> entry : pairCorpus.entrySet()) {
+            totalPairsOfWords += entry.getValue();
         }
-
-        // calculating the probability of each pair of words
+    
         Double[][] jointProbs = new Double[vocab.size()][vocab.size()];
-        for (Map.Entry<String, Integer> item_i: corpus.entrySet())
-            for (Map.Entry<String, Integer> item_j: corpus.entrySet()) {
-            // get the information of word i
-            String word_i = item_i.getKey();
-            Integer wordId_i = vocab.get(word_i);
+    
+        for (Map.Entry<String, Integer> entry_i : vocab.entrySet()) {
+            String word_i = entry_i.getKey();
+            int idx_i = entry_i.getValue();
+    
+            for (Map.Entry<String, Integer> entry_j : vocab.entrySet()) {
+                String word_j = entry_j.getKey();
+                int idx_j = entry_j.getValue();
 
-            // get the information of word j
-            String word_j = item_j.getKey();
-            Integer wordId_j = vocab.get(word_j);
-
-            if (word_i == word_j) {
-                jointProbs[wordId_i][wordId_j] = 1e-20;
-                continue;
-            }
-
-            String wordKey_ij = word_i + "_" + word_j;
-            if (pairCorpus.containsKey(wordKey_ij)) {
-                Integer wordCount_ij = pairCorpus.get(wordKey_ij);
-                jointProbs[wordId_i][wordId_j] = ((double) wordCount_ij / totalPairsOfWords);
-            }
-            else {
-                jointProbs[wordId_i][wordId_j] = 1e-20;
-            }
-
-            String wordKey_ji = word_j + "_" + word_i;
-            if (pairCorpus.containsKey(wordKey_ji)) {
-                Integer wordCount_ji = pairCorpus.get(wordKey_ji);
-                jointProbs[wordId_j][wordId_i] = ((double) wordCount_ji / totalPairsOfWords);
-            }
-            else {
-                jointProbs[wordId_j][wordId_i] = 1e-20;
+                if (word_i == word_j) {
+                    jointProbs[idx_i][idx_j] = 0.0;
+                    continue;
+                }
+    
+                String pairKey = word_i + "_" + word_j;
+    
+                if (pairCorpus.containsKey(pairKey)) {
+                    int count = pairCorpus.get(pairKey);
+                    jointProbs[idx_i][idx_j] = (double) count / totalPairsOfWords;
+                } else {
+                    jointProbs[idx_i][idx_j] = 1e-20;
+                }
             }
         }
-
-        // calculating the conditional probability of each pair of words
+    
         conditionalProbs = new Double[vocab.size()][vocab.size()];
-        for (Map.Entry<String, Integer> item_i: corpus.entrySet())
-            for (Map.Entry<String, Integer> item_j: corpus.entrySet()) {
-            // get the information of word i
-            String word_i = item_i.getKey();
-            Integer wordId_i = vocab.get(word_i);
+    
+        for (Map.Entry<String, Integer> entry_i : vocab.entrySet()) {
+            String word_i = entry_i.getKey();
+            int idx_i = entry_i.getValue();
+    
+            for (Map.Entry<String, Integer> entry_j : vocab.entrySet()) {
+                String word_j = entry_j.getKey();
+                int idx_j = entry_j.getValue();
 
-            // get the information of word j
-            String word_j = item_j.getKey();
-            Integer wordId_j = vocab.get(word_j);
-
-            // determining the P(w_i | w_j)
-
-            // determining the P(w_j | w_i)
+                String word_i_j = word_i + "_" + word_j;
+                
+                if (!pairCorpus.containsKey(word_i_j)) {
+                    conditionalProbs[idx_i][idx_j] = 1e-10;
+                    continue;
+                }
+    
+                // determining the P(w_i | w_j)
+                conditionalProbs[idx_i][idx_j] = jointProbs[idx_i][idx_j] / probs[idx_j];
+    
+                // handling edge cases
+                if (Double.isNaN(conditionalProbs[idx_i][idx_j]) || Double.isInfinite(conditionalProbs[idx_i][idx_j])) {
+                    conditionalProbs[idx_i][idx_j] = 1e-10;
+                }
+    
+                conditionalProbs[idx_j][idx_i] = jointProbs[idx_j][idx_i] / probs[idx_i];
+                if (Double.isNaN(conditionalProbs[idx_j][idx_i]) || Double.isInfinite(conditionalProbs[idx_j][idx_i])) {
+                    conditionalProbs[idx_j][idx_i] = 1e-10;
+                }
+            }
         }
     }
+    
 
     public static void training() {
         constructSingleProb();
@@ -149,37 +161,49 @@ public class B4_BayesianLM {
     }
 
     public static Vector<String> inferring(String w0) {
-        Vector<String> words = new Vector<String>();
-        words.add(w0);
+        Vector<String> res = new Vector<String>();
+        res.add(w0);
 
-        Integer w0Idx = vocab.get(w0);
-        Double logProbs = -Math.log(probs[w0Idx]);
-        for (int t = 1; t <= 5; t++) {
-            // determine the word that gives the highest probability P(w0, w1, ..., wt)
-            String maxWord = new String();
-            Double maxLogProb = 0.0;
-            Integer w1Idx = new Integer(0);
-            for (Map.Entry<String, Integer> item: vocab.entrySet()) {
-                w1Idx = item.getValue();
-                Double prob = conditionalProbs[w0Idx][w1Idx];
-                if (logProbs + (-Math.log(prob)) > logProbs + maxLogProb) {
-                    maxWord = item.getKey();
-                    maxLogProb = -Math.log(prob);
+        Integer prevIdx = vocab.get(w0);
+
+        System.out.println(prevIdx);
+
+        for (int t = 1; t < 5; t++) {
+            String nextWord = "";
+            double maxProb = -1.0;
+            int nextIdx = -1;
+
+            for (Map.Entry<String, Integer> entry : vocab.entrySet()) {
+                int candidateIdx = entry.getValue();
+                // double prob = conditionalProbs[candidateIdx][prevIdx];
+                double prob = conditionalProbs[prevIdx][candidateIdx];
+
+                if (prob > maxProb) {
+                    maxProb = prob;
+                    nextWord = entry.getKey();
+                    nextIdx = candidateIdx;
                 }
             }
-            logProbs += maxLogProb;
-            words.add(maxWord);
-            w0Idx = w1Idx;
+
+            //print nextid
+            System.out.println(nextIdx); 
+
+            // if (nextWord.equals(""))
+            //     break;
+            res.add(nextWord);
+            prevIdx = nextIdx;
         }
 
-        return words;
+        return res;
     }
 
     public static void main(String[] args) throws Exception {
         readFile();
         training();
-        Vector<String> predicted_words = inferring("hàng");
+        Vector<String> predicted_words = inferring("quay");
         String sentence = String.join(" ", predicted_words);
-        System.out.println(sentence);
+        PrintStream out = new PrintStream(System.out, true, "UTF-8");
+        out.println(sentence);
+
     }
 }
